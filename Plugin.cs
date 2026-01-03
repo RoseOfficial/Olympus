6762cd0737459c5ff5b3191eb8eadb1195649dc3
RoseOfficial <christopherscottkeller@gmail.com>
2026-01-03 18:35:25 -0500

    Fix DalamudApiLevel to 11 (v1.2.2)

--- Plugin.cs
+++ Plugin.cs
@@ -18,7 +18,7 @@ namespace Olympus;
 
 public sealed class Plugin : IDalamudPlugin
 {
-    public const string PluginVersion = "1.2.1";
+    public const string PluginVersion = "1.2.2";
     private const string CommandName = "/olympus";
 
     // Job IDs for supported classes
