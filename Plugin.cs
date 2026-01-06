b6a4f424d96c306c535c79ae8165864466424c68
RoseOfficial <christopherscottkeller@gmail.com>
2026-01-06 11:37:17 -0500

    Bump version to 1.6.3

--- Plugin.cs
+++ Plugin.cs
@@ -20,7 +20,7 @@ namespace Olympus;
 
 public sealed class Plugin : IDalamudPlugin
 {
-    public const string PluginVersion = "1.6.2";
+    public const string PluginVersion = "1.6.3";
     private const string CommandName = "/olympus";
 
     private readonly IDalamudPluginInterface pluginInterface;
