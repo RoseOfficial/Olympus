e0483fcc4a827369446c880d0efd266f646626e6
RoseOfficial <RoseOfficial@users.noreply.github.com>
2026-03-12 10:41:22 -0400

    Update repo.json for v4.9.3

--- Plugin.cs
+++ Plugin.cs
@@ -34,7 +34,7 @@ namespace Olympus;
 
 public sealed class Plugin : IDalamudPlugin
 {
-    public const string PluginVersion = "4.9.2";
+    public const string PluginVersion = "4.9.3";
     private const string CommandName = "/olympus";
 
     private readonly IDalamudPluginInterface pluginInterface;
