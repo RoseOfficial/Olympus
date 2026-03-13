98d09777ef2f8d25004ba049af19cca368c2cce1
RoseOfficial <RoseOfficial@users.noreply.github.com>
2026-03-13 09:35:46 -0400

    Add OverlayConfig with IsVisible and initial position defaults

---
+++ Config/OverlayConfig.cs
@@ -0,0 +1,8 @@
+namespace Olympus.Config;
+
+public sealed class OverlayConfig
+{
+    public bool IsVisible { get; set; } = true;
+    public float X { get; set; } = 100f;
+    public float Y { get; set; } = 100f;
+}
