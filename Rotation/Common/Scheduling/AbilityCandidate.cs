f3ee39b44f897e7244703c51e0aeb1f58fa7138f
RoseOfficial <RoseOfficial@users.noreply.github.com>
2026-04-20 13:27:00 -0400

    Add scheduler data types: AbilityBehavior, AbilityCandidate, SchedulerDispatchResult

---
+++ Rotation/Common/Scheduling/AbilityCandidate.cs
@@ -0,0 +1,16 @@
+using System;
+using Olympus.Rotation.Common;
+
+namespace Olympus.Rotation.Common.Scheduling;
+
+/// <summary>
+/// Internal queue entry. Modules construct these via <c>RotationScheduler.Push*</c>.
+/// </summary>
+internal readonly struct AbilityCandidate
+{
+    public required AbilityBehavior Behavior { get; init; }
+    public required ulong TargetId { get; init; }
+    public required int Priority { get; init; }
+    public required int InsertionOrder { get; init; }
+    public Action<IRotationContext>? OnDispatched { get; init; }
+}
